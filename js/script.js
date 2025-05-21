337ff6126d141289d1cec63af2a333eae37f11a4
jaisan <jaisanaj1999@gmail.com>
2025-05-21 18:38:20 +0200

    mobile side navbar finished and improved code quality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-const mobileMenuIcon = document.querySelector(".mobileMenuIcon");
+const mobileMenuOpen = document.querySelector(".mobileMenuOpen");
+const mobileMenuClose = document.querySelector(".mobileMenuClose");
+const overlayOpen = document.querySelector(".overlay");
 const mobilenavbar = document.querySelector(".mobilenavbar");
 
-mobileMenuIcon.addEventListener("click", () => {
-  mobilenavbar.style.right = 0;
+mobileMenuOpen.addEventListener("click", () => {
+  mobilenavbar.classList.toggle("active");
+  overlayOpen.classList.toggle("active");
+});
+mobileMenuClose.addEventListener("click", () => {
+  mobilenavbar.classList.toggle("active");
+  overlayOpen.classList.toggle("active");
 });

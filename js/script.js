91e06cabbcf87bbb7aa3e322e228aebac4b58138
jaisan <jaisanaj1999@gmail.com>
2025-05-23 02:45:28 +0200

    finished product listing main page and added cards

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ const mobilenavbar = document.querySelector(".mobilenavbar");
 mobileMenuOpen.addEventListener("click", () => {
   mobilenavbar.classList.toggle("active");
   overlayOpen.classList.toggle("active");
+  document.body.classList.toggle("noscroll");
 });
 mobileMenuClose.addEventListener("click", () => {
   mobilenavbar.classList.toggle("active");
   overlayOpen.classList.toggle("active");
+  document.body.classList.toggle("noscroll");
 });
